51945a263900f4ed40b3cdd350a7162289e95b4e
debanjanchatterjee2001 <chatterjeee.debanjan@gmail.com>
2024-06-06 20:24:58 +0530

    small change regarding title

--- src/app/cars/[make]/page.tsx
+++ src/app/cars/[make]/page.tsx
@@ -10,20 +10,11 @@ interface CarsByMakePageProps {
   };
 }
 
-const getCars = cache(async (make: string) => {
-  const cars = await prisma.car.findMany({
-    where: { make: make },
-    orderBy: { id: "desc" },
-  });
-  if (!cars) return notFound();
-  return cars;
-});
-
 export async function generateMetadata({
   params: { make },
 }: CarsByMakePageProps): Promise<Metadata> {
   const car = await prisma.car.findFirst({ where: { make: make } });
-
+  if (!car) return notFound();
   return {
     title: make,
   };
@@ -32,7 +23,11 @@ export async function generateMetadata({
 export default async function CarsByMakePage({
   params: { make },
 }: CarsByMakePageProps) {
-  const cars = await getCars(make);
+  const cars = await prisma.car.findMany({
+    where: { make: make },
+    orderBy: { id: "desc" },
+  });
+  if (!cars) return notFound();
   return (
     <div className="m-4 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {cars.map((car) => (
